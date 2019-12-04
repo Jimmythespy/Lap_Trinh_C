dbb1e82678986d63434e90f1f916b6b84be01238
Jimmythespy <56300447+Jimmythespy@users.noreply.github.com>
2019-12-04 08:07:24 +0700

    Update 2.10.c

--- Docs/Bài tập C/bai_tap_tuan_1/2.10.c
+++ Docs/Bài tập C/bai_tap_tuan_1/2.10.c
@@ -1,8 +1,14 @@
+/* De bai: nhap vao ba canh cua mot tam giac
+** viet thuat toan kiem tra xem ba canh co tao thanh mot tam giac hay khong
+** Neu co do la tam giac loai gi
+*/
+
 #include<stdio.h>
 #include<conio.h>
 #include<math.h>
 main()
 {
+	// Nhap gia tri cau 3 canh
 	int a,b,c;
 	printf("\n nhap gia tri a=");
 	scanf("%d",&a);
@@ -10,24 +16,29 @@ main()
 	scanf("%d",&b);
 	printf("\n nhap gia tri a=");
 	scanf("%d",&c);
+	// kiem tra xem 3 canh co tao thanh mot tam giac hay khong?
 	if(a+b>c||a+c>b||b+c>a)
-{
+	{
 	printf("\n day la 3 canh cua mot tam giac");
+	// Tam giac vuong
 	if(a*a+b*b==c*c||c*c+b*b==a*a||a*a+c*c==b*b)
 	{
 		printf(" vuong");
 	}
+	// Tam giac deu
 	if(a==b==c)
 	{
 		printf(" deu");
 	}
+	// Tam giac can
 	if((a==b||b==c||c==a)&&!(a==b==c))
 	{
 	printf(" can");
 	}
-}
+	}
+	// Khong phai la tam giac
 	else
-{
+	{
 	printf("day khong phai la 3 canh cua mot tam giac");
-}
+	}
 }
