8e6d8a218b223f62ffcfb460b215987db3580943
Jimmythespy <56300447+Jimmythespy@users.noreply.github.com>
2019-12-04 08:29:59 +0700

    Update 2.16.c

--- Docs/Bài tập C/bai_tap_tuan_1/2.16.c
+++ Docs/Bài tập C/bai_tap_tuan_1/2.16.c
@@ -1,23 +1,26 @@
+/* DE BAI: Tinh gia tri cua pi
+** giua tren gia tri cua epsilon
+*/
 #include<stdio.h>
 #include<conio.h>
 #include<math.h>
 main()
 {
 	int i;
-	float pi=0,e;
+	float pi=0,e; // Nhap gia tri cua epsilon
 	printf(" nhap gia tri cua epsilon (0<epsilon<1)\n epsilon=");
-		scanf("%f",&e);
-		printf(" dang xu ly xin cho:");
-		if(e>1||e<0){
-			printf(" gia tri cua epsilon khong hop le\n Bam phim bat ky de ket thuc:");
-			getch();
-			return 0;
+	scanf("%f",&e);
+	if(e>1||e<0)
+	{
+		printf(" gia tri cua epsilon khong hop le\n Bam phim bat ky de ket thuc:");
+		return 0;
 		}
+	// Vong lap de tinh pi
 	do
 	{
 		i=1;
 		pi=pi+(pow(-1,i)*(1/2*i+1));
-	}while((i/(2*i+1))<=e);
+		}while((i/(2*i+1))<=e);
 	pi=pi/4;
 	printf("\n gia tri cua pi=%f",pi);
 	getch();
