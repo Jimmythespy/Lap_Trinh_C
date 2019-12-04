a5edc900876e2f934a45f0e2c82f2b6005629323
Jimmythespy <56300447+Jimmythespy@users.noreply.github.com>
2019-12-04 08:35:53 +0700

    Update 2.19.c

--- Docs/Bài tập C/bai_tap_tuan_1/2.19.c
+++ Docs/Bài tập C/bai_tap_tuan_1/2.19.c
@@ -5,18 +5,27 @@
 #include<math.h>
 main()
 {
-	int t,n,sum=0,remainder; printf(" Nhap gia tri cua n nguyen (n<1000):\n n= "); scanf("%d",&n);
-	char *ques; ques="y";
+	int t,n,sum=0,remainder; // remainder phan du
+	printf(" Nhap gia tri cua n nguyen (n<1000):\n n= "); 
+	scanf("%d",&n);
+	char *ques; 
+	ques="y"; // Bien cau hoi
 	while((n>=10000||n<=0)&&(ques="y"))
 	{
-	printf("\n gia tri cua n khong hop le\n Ban co muon nhap lai khong?\n (y/n):");
-	scanf("%s",&ques);
-	if(ques!="y") // Sai o cho nay
-	{ printf("\n Bam phim bat ki de ket thuc:"); return 0;	}
-	}  
+		printf("\n gia tri cua n khong hop le\n Ban co muon nhap lai khong?\n (y/n):");
+		scanf("%s",&ques);
+	// Ung voi cau tra loi khong
+	if(ques='n') // 
+	{ 
+		printf("\n Bam phim bat ki de ket thuc:"); return 0;	}
+	} 
+	// Tinh tong cac chua so
 	t = n;
-	while (t != 0) {remainder = t % 10;
-      sum       = sum + remainder; 
-      t         = t / 10;}
+	while (t != 0) 
+	{
+		remainder = t % 10;
+      		sum = sum + remainder; 
+      		t = t / 10;
+	}
 	printf(" tong cua cac chu so cua %d=%d",n,sum);      
 }
