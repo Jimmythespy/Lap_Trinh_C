872ccc55d4a5f9a066371b1b1ce42d22970e64ad
Jimmythespy <56300447+Jimmythespy@users.noreply.github.com>
2019-12-04 08:42:37 +0700

    Update bai 5.c

--- Docs/Bài tập C/bai_tap_tuan_1/bai 5.c
+++ Docs/Bài tập C/bai_tap_tuan_1/bai 5.c
@@ -5,28 +5,33 @@ tinh tong S=1+2+3+...+n
 
 #include <stdio.h>
 #include <conio.h>
- main()
- {
- int n,T,i;
- char ques;
- ques='y';
-  do {
-  printf(" Nhap so nguyen 10<n<100 : ");
+main()
+{
+	// Nhap gia tri cua n
+	// Bien T
+	int n,T,i;
+	char ques;
+	ques='y';
+	do 
+	{
+		printf(" Nhap so nguyen 10<n<100 : ");
 		scanf("%d",&n);
-	  if(n<=10||n>=100)
-	  {
-	  	printf(" gia tri cua n khong hop le");
-	  	printf("\n ban co muon nhap lai khong? ");
-	  	printf("\n neu co nhap y\n neu khong nhap n\n (y/n) ");
-	  	fflush(stdin);
-		scanf("%c",&ques);
-	  }
-	  }
-  while((n<=10||n>=100)&&(ques!='n')); 
-  if(ques='n')
-  	return 0;
-  T=0;
-  for (i=0;i<n;i++) T=T+i+1;
-  printf("\n Tong tu 1 den n la : %d",T);
-  getch();
+		if(n<=10||n>=100)
+	  	{
+	  		printf(" gia tri cua n khong hop le");
+	  		printf("\n ban co muon nhap lai khong? ");
+	  		printf("\n neu co nhap y\n neu khong nhap n\n (y/n) ");
+			scanf("%c",&ques);
+	  	}
+	 }
+	// Cau tra loi
+	while((n<=10||n>=100)&&(ques!='n'))
+	{
+		if(ques='n')
+		return 0;
+	}
+	T=0;
+	for (i=0;i<n;i++) T=T+i+1;
+	printf("\n Tong tu 1 den n la : %d",T);
+	getch();
 }
